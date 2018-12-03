7156f435bc3c15dcc80c5aba218d6a2929b5d707
Julian <juli11c0@edu.eal.dk>
2018-12-03 09:41:14 +0100

    Structur

--- Adt/MyLinkedList.cs
+++ Adt/MyLinkedList.cs
@@ -8,5 +8,42 @@ namespace Adt
 {
 	class MyLinkedList
 	{
+		private int count;
+
+		public int Count { get {return this.count; } set {this.count = value; } }
+
+
+
+		public void Insert(object m)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Insert(object m, int b)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete()
+		{
+			throw new NotImplementedException();
+		}
+
+
+		public void Delete(int b)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Node ItemAt(int b)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override string ToString()
+		{
+			throw new NotImplementedException();
+		}
+
 	}
 }
---
+++ ex32-linkedlist/CLubMember.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex32_linkedlist
+{
+	class ClubMember
+	{
+		private int nr;
+		private string fnavn;
+		private string lnavn;
+		private int alder;
+
+		public int Nr { get; set; }
+		public string FNavn { get; set;}
+		public string LNavn { get; set; }
+		public int Alder { get; set; }
+
+
+		public ClubMember(int Nr, string FNavn, string LNavn, int Alder)
+		{
+			nr = Nr;
+			fnavn = FNavn;
+			lnavn = LNavn;
+			alder = Alder;
+		}
+
+
+		public override string ToString()
+		{
+			return "Nr: " + nr + "FNavn: " + fnavn + "LNavn: " + lnavn + "Alder: " + alder;
+		}
+	}
+}
