7b9176a02df0a8ad9f3e36d830cd6ddd18667a8c
Julian <juli11c0@edu.eal.dk>
2018-12-03 09:41:24 +0100

    Structur

--- ex32-linkedlist/ClubMember.cs
+++ ex32-linkedlist/ClubMember.cs
@@ -8,5 +8,29 @@ namespace ex32_linkedlist
 {
 	class ClubMember
 	{
+		private int nr;
+		private string fnavn;
+		private string lnavn;
+		private int alder;
+
+		public int Nr { get; set; }
+		public string FNavn { get; set;}
+		public string LNavn { get; set; }
+		public int Alder { get; set; }
+
+
+		public ClubMember(int Nr, string FNavn, string LNavn, int Alder)
+		{
+			nr = Nr;
+			fnavn = FNavn;
+			lnavn = LNavn;
+			alder = Alder;
+		}
+
+
+		public override string ToString()
+		{
+			return "Nr: " + nr + "FNavn: " + fnavn + "LNavn: " + lnavn + "Alder: " + alder;
+		}
 	}
 }
